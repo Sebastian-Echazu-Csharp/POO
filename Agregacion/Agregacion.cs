7625a35bdca45219fee5bd1bb861e572a20b9092
Sebastián Echazú <sebastianechazu@outlook.com>
2020-02-12 18:50:37 -0300

    agrege el ejemplo de agregacion

---
+++ Agregacion/Agregacion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agregacion
+{
+    class Edificio
+    {
+        private string Direccion { get; set; }
+        private int Pisos { get; set; }
+
+        public Edificio(string Direccion, int Pisos)
+        {
+            this.Direccion = Direccion;
+            this.Pisos = Pisos;
+
+        }
+        public void Muestra()
+        {
+            Console.WriteLine("El edificio se encuentra en {0} y tiene {1} pisos", Direccion, Pisos);
+
+        }
+    }
+
+    class Propietario
+    {
+
+        private string Nombre { get; set; }
+
+        private Edificio edificio = null;
+
+        public Propietario(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public void MuestraPropiedad()
+        {
+            if (edificio != null)
+            {
+                Console.Write("{0} tiene ", Nombre);
+                edificio.Muestra();
+            }
+            else
+            {
+                Console.WriteLine("{0} aun no tiene propiedades ", Nombre);
+            }
+
+        }
+        public void AdicionaEdificio(Edificio edificio)
+        {
+            if (edificio != null)
+            {
+                this.edificio = edificio;
+
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Agregacion;
 using Herencia;
+
 
 namespace POO
 {
@@ -8,18 +10,38 @@ namespace POO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prueba de clases");
+            Console.WriteLine("Prueba de Programacion Orientada a Objetos");
 
+            //-------------------------------------------------------------------------------------
+            //HERENCIA
+            //-------------------------------------------------------------------------------------
+            //Creaos una lista de personas
             List<Persona> Personas = new List<Persona>();
-
-            Personas.Add(new Profesor("Programacion","Sebastian","Echazu"));
-            Personas.Add(new Alumno("5to C","Juan","Perez"));
-            Personas.Add(new Alumno("5to C","Pedro","Gonzales"));
-
+            //a esa lista de personas le agregamos alumnos y clases
+            Personas.Add(new Profesor("Programacion", "Sebastian", "Echazu"));
+            Personas.Add(new Alumno("5to C", "Juan", "Perez"));
+            Personas.Add(new Alumno("5to C", "Pedro", "Gonzales"));
+            //iteramos en la lista de persona mostrando la informacion
             foreach (var persona in Personas)
             {
                 persona.EstaEnClase();
             }
+            //-------------------------------------------------------------------------------------
+            //AGREGACION
+            //-------------------------------------------------------------------------------------
+            //creamos un edificio
+            Edificio ed = new Edificio("San martin 844", 8);
+            //creamos un propietario
+            Propietario pro = new Propietario("sebastian");
+            //mostramos el la propiedades del edicifio
+            pro.MuestraPropiedad();
+            //agregamos la propiedad al propietario
+            pro.AdicionaEdificio(ed);
+            //mostramos las propiedades del propietario
+            pro.MuestraPropiedad();
+
+
+
         }
     }
 }
